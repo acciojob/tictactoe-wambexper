70379d04bfa587fd339399a1a8c00b791d0f1996
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-08 13:15:51 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,52 @@
-let input1=document.getElementById("player-1");
-let input2=document.getElementById("player-2");
-let btn=document.getElementById("submit");
-let grid=document.getElementById("grid");
-let main=document.getElementsByClassName("container");
-let row=document.getElementsByClassName("row");
-let msg=document.getElementsByClassName("message");
-let c=document.getElementsByClassName("cont");
-btn.addEventListener("click",()=>{
-    if(input1.value===""){
-        alert("please enter name")
+//your JS code here. If required.
+let currentPlayer = "X";
+let isGameActive = false;
+let board = ["", "", "", "", "", "", "", "", ""];
+
+function startGame() {
+    const player1Name = document.getElementById("player-1").value;
+    const player2Name = document.getElementById("player-2").value;
+    document.getElementById("message").textContent = `${player1Name}, you're up`;
+    isGameActive = true;
+}
+
+function handleClick(cell) {
+    const index = cell.id - 1;
+
+    if (board[index] === "" && isGameActive) {
+        cell.textContent = currentPlayer;
+        board[index] = currentPlayer;
+        checkWin();
+        currentPlayer = currentPlayer === "X" ? "O" : "X";
+        document.getElementById("message").textContent = `${currentPlayer === "X" ? player1Name : player2Name}, you're up`;
     }
-    else if(input2.value===""){
-         alert("please enter name");
-    }
-    else{
-    main[0].className="display";
-   
-    for(let i=0;i<9;i++){
-    let div=document.createElement("td");
-    div.className="box";
-    div.id=i;
-    let num=Math.floor(i/3);
-   row[num].appendChild(div);
-   
-    }
-    }
-    let count=1;
-      msg[0].textContent=`${input1.value},you're up`
-    c[0].addEventListener("click",(e)=>{
-        if(count%2!==0 && e.target.textContent===""){
-        e.target.textContent="X";
-         msg[0].textContent=`${input.value},you're up`
-        count++;
+}
+
+function checkWin() {
+    const winPatterns = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    for (const pattern of winPatterns) {
+        const [a, b, c] = pattern;
+        if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+            document.getElementById("message").textContent = `${currentPlayer === "X" ? player2Name : player1Name}, congratulations you won!`;
+            isGameActive = false;
         }
-        else if( e.target.textContent===""){
-             e.target.textContent="O";
-             count++;
-              msg[0].textContent=`${input1.value},you're up`
-        }
-    })
-    
-});
+    }
+
+    if (!board.includes("") && isGameActive) {
+        document.getElementById("message").textContent = "It's a draw!";
+        isGameActive = false;
+    }
+}
+
+const cells = document.querySelectorAll(".cell");
+cells.forEach(cell => cell.addEventListener("click", () => handleClick(cell)));
